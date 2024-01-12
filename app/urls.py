98a4d75ab818c949f54e47644c487f124d2118e1
stefanus_daeli <stefanudaeli2003@gamil.com>
2024-01-12 09:23:22 +0700

    created v_owners

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns =[
-    path('index',index,name='index')
+    path('index',index,name='index'),
+    
+    #login
+    path('v_owners',v_owners,name='v_owners')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
     context ={
         'name' : 'stefan'
     }
     return render(request,'index.html',context)
+
+def v_owners(request):
+    data_owners = Owners.objects.all().order_by('owner_id')
+    context = {
+        'data_owners' : data_owners
+    }
+    return render (request,'owners/v_owners.html',context)
